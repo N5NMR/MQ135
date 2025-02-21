1fdbed0d1c20e5b069055accf7f6c53c1c6d6d9a
N5NMR <131434371+N5NMR@users.noreply.github.com>
2025-02-21 16:25:11 +0100

    Add files via upload

--- UtilityPrg_MQ135__NokiaDisplay.ino
+++ UtilityPrg_MQ135__NokiaDisplay.ino
@@ -4,9 +4,9 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
  
-#define SCREEN_WIDTH 128    // OLED display width, in pixels
-#define SCREEN_HEIGHT 64    // OLED display height, in pixels
-#define OLED_RESET -1       // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_WIDTH 128  
+#define SCREEN_HEIGHT 64   
+#define OLED_RESET -1    
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 float air_quality; 
@@ -15,17 +15,17 @@ String air_Index;
 void setup()
 {
   Serial.begin(9600);
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3C); //initialize with the I2C addr 0x3C (128x64)
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C); 
   display.clearDisplay();
   delay(10);
-    display.setCursor(0,0);  
-    display.setTextSize(1);
-    display.setTextColor(WHITE);
-    display.println("Starte Messungen...");
-    display.println("");
-    display.println("MQ 135");
-    display.display();
-    delay(4000);
+  display.setCursor(0,0);  
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.println("Starte Messungen...");
+  display.println("");
+  display.println("MQ 135");
+  display.display();
+  delay(4000);
 }
  
  
@@ -41,24 +41,23 @@ void setup()
     Serial.println("  PPM");   
  
     display.clearDisplay();
-    display.setCursor(0,0);  //oled display
+    display.setCursor(0,0);  
     display.setTextSize(1);
     display.setTextColor(WHITE);
     display.println("Air Quality Index");
     
-  
-    display.setCursor(0,20);  //oled display
+    display.setCursor(0,20);  
     display.setTextSize(2);
     display.setTextColor(WHITE);
     display.print(air_quality);
     display.setTextSize(1);
     display.setTextColor(WHITE);
     display.println(" PPM");
-    display.setCursor(0,40);  //oled display
+    display.setCursor(0,40);  
     display.print("Luft ist: ");
-    display.setCursor(0,50);  //oled display
+    display.setCursor(0,50);  
     display.println(air_Index);
     display.display();
  
-    delay(2000);      // thingspeak needs minimum 15 sec delay between updates.
+    delay(2000);
 }
